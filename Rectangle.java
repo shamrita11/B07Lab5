e96d5298b4b8dc6de4967851cea7825ac05b3139
Shahad Osman <shahad.osman@mail.utoronto.ca>
2024-10-23 13:48:00 -0400

    feat: created Rectangle class implementing perimeter() and isSquare() methods

---
+++ Rectangle.java
@@ -0,0 +1,30 @@
+package B07Lab5;
+
+public class Rectangle {
+	Point A;
+	Point B;
+	Point C;
+	Point D;
+	
+	public Rectangle(Point A, Point B, Point C, Point D) {
+		this.A = A;
+		this.B = B;
+		this.C = C;
+		this.D = D;
+	}
+	
+	public double perimeter() {
+		return  A.distance(B) + B.distance(C) + C.distance(D) + D.distance(A);
+	}
+	
+	public boolean isSquare() {
+		double dAB = A.distance(B);
+		double dBC = B.distance(C);
+		double dCD = C.distance(D);
+		double dDA = D.distance(A);
+		if(dAB==dBC && dBC==dCD && dCD == dDA)
+			return true;
+		else
+			return false;
+	}
+}
